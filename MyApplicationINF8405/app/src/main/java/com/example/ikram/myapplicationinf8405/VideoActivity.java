000d99453e42ed70a020bdc458e4f578cbd6a9c0
Jackie Phung <jackie.phung@polymtl.ca>
2018-01-24 15:29:38 -0500

    accelerometer added

--- MyApplicationINF8405/app/src/main/java/com/example/ikram/myapplicationinf8405/VideoActivity.java
+++ MyApplicationINF8405/app/src/main/java/com/example/ikram/myapplicationinf8405/VideoActivity.java
@@ -1,9 +1,15 @@
 package com.example.ikram.myapplicationinf8405;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -19,12 +25,33 @@ public class VideoActivity extends Activity {
     private VideoView mv;
     String URL = "";
 
+    //declare accelerometer text view
+    //TextView textAccesX, textAccesY, textAccesZ;
+
+    //declare sensors
+    SensorManager sensorManager;
+    Sensor sensorAccelerometer;
+
+    //assign initial values to gravity, acceleration and alpha
+    double[] gravity = {0, 0, 0};
+    double[] linear_acceleration = {0, 0, 0};
+    double alpha = 0.8;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
         mv = new VideoView(this);
         setContentView(mv);
+
+        //set up sensors and accelerometer
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //get the text view of X,Y,Z
+        /*textAccesX = findViewById(R.id.accesX);
+        textAccesY = findViewById(R.id.accesY);
+        textAccesZ = findViewById(R.id.accesZ);*/
 
         Bundle extras = getIntent().getExtras();
         if (extras != null && !extras.getString("ip").equals("")) {
@@ -36,11 +63,49 @@ public class VideoActivity extends Activity {
         new DoRead().execute(URL);
     }
 
+    //On resume, register accelerometer listener
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(accelerometerListener, sensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
     public void onPause() {
         super.onPause();
         mv.stopPlayback();
         finish();
     }
+
+    //On stop , unregister accelerometer listener
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(accelerometerListener);
+    }
+
+    //Accelerometer listener, set the values to the text view
+    public SensorEventListener accelerometerListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) { }
+
+        public void onSensorChanged(SensorEvent event) {
+
+            // Isolate the force of gravity with the low-pass filter.
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+            // Remove the gravity contribution with the high-pass filter.
+            linear_acceleration[0] = event.values[0] ;//- gravity[0];
+            linear_acceleration[1] = event.values[1] ;//- gravity[1];
+            linear_acceleration[2] = event.values[2] ;//- gravity[2];
+
+            Log.d("X", "X : " + (int)linear_acceleration[0] + " m/s^2");
+            Log.d("Y", "Y : " + (int)linear_acceleration[1] + " m/s^2");
+            Log.d("Z", "Z : " + (int)linear_acceleration[2] + " m/s^2");
+
+            //textAccesX.setText("X : " + (int)linear_acceleration[0] + " m/s^2");
+            //textAccesY.setText("Y : " + (int)linear_acceleration[1] + " m/s^2");
+            //textAccesZ.setText("Z : " + (int)linear_acceleration[2] + " m/s^2");
+        }
+    };
 
     public class DoRead extends AsyncTask<String, Void, VideoStreamActivity> {
         protected VideoStreamActivity doInBackground(String... url) {
