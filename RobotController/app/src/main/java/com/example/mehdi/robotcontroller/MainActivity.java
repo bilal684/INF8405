6ccf22dab2ed626f2ebb6a03c5e1c7f917c17e83
Mehdi Kadi <mehdi.kadi@polymtl.com>
2018-02-01 00:02:36 -0500

    client pour controller le robot

---
+++ RobotController/app/src/main/java/com/example/mehdi/robotcontroller/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.mehdi.robotcontroller;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int SERVERPORT = 9999;
+    public static final String SERVER_IP = "192.168.1.218";
+
+    ClientThread clientThread;
+    Thread thread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clientThread = new ClientThread();
+        thread = new Thread(clientThread);
+
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        thread.start();
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        // to implement
+    }
+
+    // source : https://stackoverflow.com/questions/25093546/android-os-networkonmainthreadexception-at-android-os-strictmodeandroidblockgua
+    // source : http://www.coderzheaven.com/2017/05/01/client-server-programming-in-android-send-message-to-the-client-and-back/
+    class ClientThread implements Runnable {
+
+        private Socket socket;
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        void sendMessage(String message) {
+            try {
+                if (null != socket) {
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                    out.write(message);
+                    out.flush();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != clientThread) {
+            clientThread.sendMessage("Disconnect");
+            clientThread = null;
+        }
+    }
+
+    public void W(View view)
+    {
+        clientThread.sendMessage("w");
+    }
+
+    public void S(View view)
+    {
+        clientThread.sendMessage("s");
+    }
+
+    public void A(View view)
+    {
+        clientThread.sendMessage("a");
+    }
+
+    public void D(View view)
+    {
+        clientThread.sendMessage("d");
+    }
+
+    public void X(View view)
+    {
+        clientThread.sendMessage("x");
+    }
+}
