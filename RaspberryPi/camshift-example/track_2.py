1a13ec8e7bc737bac7ff162933a03c6910173954
bilal itani <bilal.itani1234@gmail.com>
2018-04-13 11:12:51 -0400

    test

--- RaspberryPi/camshift-example/track_2.py
+++ RaspberryPi/camshift-example/track_2.py
@@ -1,11 +1,6 @@
-# USAGE
-# python track.py --video video/sample.mov
-
-# import the necessary packages
 import numpy as np
 import argparse
 import cv2
-#from picamera import PiCamera
 import time
 
 # initialize the current frame of the video, along with the list of
@@ -15,34 +10,9 @@ roiPts = [(189,101), (192, 298), (411, 302), (408, 105)]
 inputMode = False
 isInit = False
 
-#def selectROI(event, x, y, flags, param):
-	# grab the reference to the current frame, list of ROI
-	# points and whether or not it is ROI selection mode
-#	global frame, roiPts, inputMode
-
-	# if we are in ROI selection mode, the mouse was clicked,
-	# and we do not already have four points, then update the
-	# list of ROI points with the (x, y) location of the click
-	# and draw the circle
-#	if inputMode and event == cv2.EVENT_LBUTTONDOWN and len(roiPts) < 4:
-#		print(x + ";" + y)
-#		roiPts.append((x, y))
-#		cv2.circle(frame, (x, y), 4, (0, 255, 0), 2)
-#		cv2.imshow("frame", frame)
-
 def init(frame):
-	# indicate that we are in input mode and clone the
-	# frame
-	#inputMode = True
 	global roiHist, roiBox, roiPts, isInit
 	orig = frame.copy()
-
-	# keep looping until 4 reference ROI points have
-	# been selected; press any key to exit ROI selction
-	# mode once 4 points have been selected
-	#while len(roiPts) < 4:
-	#	cv2.imshow("frame", frame)
-	#	cv2.waitKey(0)
 
 	# determine the top-left and bottom-right points
 	roiPts = np.array(roiPts)
@@ -74,34 +44,23 @@ def main():
 	# grab the reference to the current frame, list of ROI
 	# points and whether or not it is ROI selection mode
 	global frame, roiPts, inputMode, roiBox, isInit
-
-	# if the video path was not supplied, grab the reference to the
-	# camera
-	#if not args.get("video", False):
-	#	camera = cv2.VideoCapture(0)
-
-	# otherwise, load the video
-	#else:
+	
 	camera = cv2.VideoCapture(0)
 
 	# setup the mouse callback
 	cv2.namedWindow("frame")
-	#cv2.setMouseCallback("frame", selectROI)
-
+	
 	# initialize the termination criteria for cam shift, indicating
 	# a maximum of ten iterations or movement by a least one pixel
 	# along with the bounding box of the ROI
 	termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
-	#(grabbed, readFrame) = camera.read()
-	#init(readFrame)
-	counter = 0
+	#counter = 0
 
-	# keep looping over the frames
 	while True:
-		if not isInit and counter == 10:
-			(grabbed, readFrame) = camera.read()
-			frameInit = cv2.flip(readFrame, -1)
-			init(frameInit)
+		#if not isInit and counter == 10:
+		#	(grabbed, readFrame) = camera.read()
+		#	frameInit = cv2.flip(readFrame, -1)
+		#	init(frameInit)
 		if isInit:
 			# grab the current frame
 			(grabbed, readFrame) = camera.read()
@@ -128,50 +87,17 @@ def main():
 			cv2.imshow("frame",frame)
 			key = cv2.waitKey(1) & 0xFF
 
-			# handle if the 'i' key is pressed, then go into ROI
-			# selection mode
-			#if key == ord("i") and len(roiPts) < 4:
-				# indicate that we are in input mode and clone the
-				# frame
-			#	inputMode = True
-			#	orig = frame.copy()
-
-				# keep looping until 4 reference ROI points have
-				# been selected; press any key to exit ROI selction
-				# mode once 4 points have been selected
-			#	while len(roiPts) < 4:
-			#		cv2.imshow("frame", frame)
-			#		cv2.waitKey(0)
-
-				# determine the top-left and bottom-right points
-			#	roiPts = np.array(roiPts)
-			#	s = roiPts.sum(axis = 1)
-			#	tl = roiPts[np.argmin(s)]
-			#	br = roiPts[np.argmax(s)]
-
-				# grab the ROI for the bounding box and convert it
-				# to the HSV color space
-			#	roi = orig[tl[1]:br[1], tl[0]:br[0]]
-			#	roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-				#roi = cv2.cvtColor(roi, cv2.COLOR_BGR2LAB)
-
-				# compute a HSV histogram for the ROI and store the
-				# bounding box
-			#	roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
-			#	roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
-			#	roiBox = (tl[0], tl[1], br[0], br[1])
-
-			# if the 'q' key is pressed, stop the loop
 			if key == ord("q"):
 				break
 		else:
-                    (grabbed, readFrame) = camera.read()
-                    frame = cv2.flip(readFrame, -1)
-                    cv2.imshow("frame",readFrame)
-                    counter = counter + 1
-
-
-	# cleanup the camera and close any open windows
+			(grabbed, readFrame) = camera.read()
+			frameInit = cv2.flip(readFrame, -1)
+			init(frameInit)
+		#else:
+         #           (grabbed, readFrame) = camera.read()
+          #          frame = cv2.flip(readFrame, -1)
+           #         cv2.imshow("frame",readFrame)
+                    #counter = counter + 1
 	camera.release()
 	cv2.destroyAllWindows()
 
